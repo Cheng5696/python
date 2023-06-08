71702f608d994c8e85401705cb16109a828b4c95
chengcheng <2278246480@qq.com>
2023-06-08 20:00:32 +0800

    6-8 -4

--- 文件选择器.py
+++ 文件选择器.py
@@ -1,4 +1,3 @@
-import jieba
 import tkinter as tk
 from tkinter import filedialog
 
@@ -8,17 +7,4 @@ root.withdraw()
 # FolderPath=filedialog.askdirectory() #如果有特殊需要，非要选择文件夹，这个可以去掉注释使用
 FilePath=filedialog.askopenfilename() #一般这个直接选择文件，会比较符合人们的使用习惯和软件的用户体验
 
-path = FilePath.replace("\\","/")
 
-
-with open(path,"r",encoding="utf-8") as f:
-    li = jieba.lcut(f.read())
-    count = {}
-    for i in li:
-        if len(i) == 1:
-            continue
-        else:
-            count[i] = count.get(i,0) + 1
-    li1 = list(count.items())
-    li1.sort(key=lambda x:x[1],reverse=True)
-    print(li1)
